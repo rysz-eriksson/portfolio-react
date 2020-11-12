3e556899774571d39f09a599c96ac9d5705ebf56
Ryszard Wolosiuk <Ryszard_Wolosiuk@epam.com>
2020-11-12 10:21:00 +0100

    add: change the info about projects

--- src/data/projectsInfo.js
+++ src/data/projectsInfo.js
@@ -1,4 +1,11 @@
 export default [{
+	title: 'Weather App',
+	description: 'Implementation of website displaying weather conditions and map info based on location or search (including voice search)',
+	technologies: ['TypeScript', 'REST API', 'Browser API', 'Webpack', 'async programming', 'Netlify'],
+	pageUrl: 'https://angry-wozniak-98baef.netlify.app/',
+    codeUrl: 'https://github.com/rysz-eriksson/weather-app',
+    imageUrl: '/images/weather-app.png'
+}, {
 	title: 'Conference CMS',
 	description: 'Design and implementation of Content Management System to update database and fetch info from it in real-time',
 	technologies: ['React', 'Hooks', 'Styled components', 'auth with back-end', 'REST API', 'Firebase', 'NoSQL database'],
@@ -12,13 +19,6 @@ export default [{
 	pageUrl: 'http://rysz-react-chat.herokuapp.com/',
     codeUrl: 'https://github.com/rysz-eriksson/react-chat-app',
     imageUrl: '/images/react-chat-app.png',
-}, {
-	title: 'Weather App',
-	description: 'Implementation of website displaying weather conditions and map info based on location or search (including voice search)',
-	technologies: ['JavaScript', 'REST API', 'Browser API', 'Webpack', 'async programming', 'Netlify'],
-	pageUrl: 'https://angry-wozniak-98baef.netlify.app/',
-    codeUrl: 'https://github.com/rysz-eriksson/weather-app',
-    imageUrl: '/images/weather-app.png'
 }, {
 	title: 'E-commerce shop',
 	description: 'Design and implementation of online shop prototype',
